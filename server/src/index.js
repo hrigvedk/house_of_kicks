9e1f11b9738bc9bd491591e334354f499be87a45
hrigvedk <khatavkar.h@northeastern.edu>
2023-12-04 14:37:28 -0500

    CORS Error

--- server/src/index.js
+++ server/src/index.js
@@ -12,6 +12,13 @@ const port = process.env.PORT || 8000
 app.use(express.json())
 app.use(userRouter)
 app.use(sneakerRouter)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.removeHeader('x-powered-by');
+    res.setHeader('Access-Control-Allow-Methods','POST');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    next();
+  });
 app.use(cors())
 
 app.listen(port, () => {
