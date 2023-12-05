4db40663da19a38d0b5bd06adddd1b4b33ff65b5
hrigvedk <khatavkar.h@northeastern.edu>
2023-12-05 18:57:13 -0500

    Authentication Text Toggle

--- client/src/components/LoginSignUpCard/LoginSignUpCard.js
+++ client/src/components/LoginSignUpCard/LoginSignUpCard.js
@@ -1,5 +1,5 @@
+// 
 import React, { useState } from 'react';
-import { Tab, Tabs, Card } from 'react-bootstrap';
 import { FaShoePrints } from 'react-icons/fa';
 import SignUp from '../SignUp/SignUp'; 
 import Login from '../Login/Login'; 
@@ -14,34 +14,32 @@ const LoginSignUpCard = () => {
 
   return (
     <>
-
       <div className="text-center mt-5">
         <h1 className='mb-4'>
           <FaShoePrints className="mr-2" />
           House of Kicks
         </h1>
       </div>
-    <div className="auth-container">
-      <Card className="auth-card">
-        <Tabs
-          id="authentication-tabs"
-          activeKey={activeTab}
-          onSelect={(tab) => handleTabChange(tab)}
-          variant="pills"
-          className="mb-3"
-        >
-          <Tab  eventKey="signup" title="Sign Up">
-           <SignUp/>
-           
-          </Tab>
-          <Tab  eventKey="login" title="Login">
-          <Login/>
-          
-          </Tab>
-        </Tabs>
-      </Card>
-    </div>
-
+      <div className="auth-container">
+        <div className="auth-card">
+          <div className="tab-toggles">
+            <span
+              className={activeTab === 'signup' ? 'active-tab' : ''}
+              onClick={() => handleTabChange('signup')}
+            >
+              Sign Up
+            </span>
+            <span
+              className={activeTab === 'login' ? 'active-tab' : ''}
+              onClick={() => handleTabChange('login')}
+            >
+              Login
+            </span>
+          </div>
+          {activeTab === 'signup' && <SignUp />}
+          {activeTab === 'login' && <Login />}
+        </div>
+      </div>
     </>
   );
 };
